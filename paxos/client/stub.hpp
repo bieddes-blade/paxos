1f4451d1d7703dac0a532d7a4a51820450754c86
Yulia Rovnik <32664910+bieddes-blade@users.noreply.github.com>
2022-11-17 16:42:47 +0300

    Add files via upload

--- paxos/client/stub.hpp
+++ paxos/client/stub.hpp
@@ -1,1 +1,33 @@
+#pragma once
 
+#include <consensus/value.hpp>
+
+#include <whirl/rpc/use/channel.hpp>
+
+#include <await/fibers/sync/future.hpp>
+#include <await/fibers/core/await.hpp>
+
+namespace paxos {
+
+using consensus::Value;
+
+using await::fibers::Await;
+using whirl::rpc::TChannel;
+
+//////////////////////////////////////////////////////////////////////
+
+class PaxosBlockingStub {
+ public:
+  explicit PaxosBlockingStub(TChannel& channel) : channel_(channel) {
+  }
+
+  Value Propose(Value value) {
+    auto result = Await(channel_.Call("Consensus.Propose", value).As<Value>());
+    return result.Value();
+  }
+
+ private:
+  TChannel& channel_;
+};
+
+}  // namespace paxos
